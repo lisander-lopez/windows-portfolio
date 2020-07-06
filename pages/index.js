3e0264f9b8cbd35a76777cb60ba878998b0e3040
Lisander Lopez <lisander@buffalo.edu>
2020-07-05 22:39:20 -0400

    Added Css Grid #5

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Head from "next/head";
+import styles from "../css/index.module.scss";
+
 import { Fragment } from "react";
+//6 Rows
 export default function Home() {
 	return (
 		<Fragment>
 			<Head>
-				<title>Create Next App - Lisander Lopez</title>
+				<title>Desktop - Lisander Lopez</title>
 			</Head>
-			<h1>Hello</h1>
+			<div className={styles["grid-container"]}>
+				<div className={styles["grid-project"]}>Project Goes here</div>
+				<div className={styles["grid-resume"]}>Resume goes here</div>
+				<div className={styles["grid-trash"]}> Trash goes here</div>
+				<div className={styles["grid-github"]}>Git hub goes here</div>
+				<div className={styles["grid-linkedin"]}> Linked in goes here</div>
+				<div className={styles["grid-email"]}> email goes here</div>
+			</div>
 		</Fragment>
 	);
 }

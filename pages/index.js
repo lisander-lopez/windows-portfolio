fb2a4e4739176b8ce8a44710c777abaa1eb924cb
Lisander Lopez <lisander@buffalo.edu>
2020-07-05 23:02:27 -0400

    Added Font Awesome #1

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from "next/head";
 import styles from "../css/index.module.scss";
 
 import { Fragment } from "react";
-//6 Rows
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+
 export default function Home() {
 	return (
 		<Fragment>
@@ -10,7 +13,12 @@ export default function Home() {
 				<title>Desktop - Lisander Lopez</title>
 			</Head>
 			<div className={styles["grid-container"]}>
-				<div className={styles["grid-project"]}>Project Goes here</div>
+				<div className={styles["grid-project"]}>
+					<div className={styles["grid-icon"]}>
+						<FontAwesomeIcon icon={faFolder} />
+						<span>Projects</span>
+					</div>
+				</div>
 				<div className={styles["grid-resume"]}>Resume goes here</div>
 				<div className={styles["grid-trash"]}> Trash goes here</div>
 				<div className={styles["grid-github"]}>Git hub goes here</div>

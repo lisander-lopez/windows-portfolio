93ebb02e07da1f3c8560367a45091313f310980f
Lisander Lopez <lisander@buffalo.edu>
2020-07-10 23:25:16 -0400

    Added windows logo #2

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -9,8 +9,14 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<div className={this.props.className + " " + styles["main-container"]}>
-				<div className={styles["left-foot"]}>Logo and Search Bar</div>
-				<div className={styles["middle-foot"]}>App Bar</div>
+				<div className={styles["left-foot"]}>
+					<div className={styles["logo-wrapper"]}>
+						<img
+							className={styles["win-logo"]}
+							src="/images/windowsLogo.png"
+						></img>
+					</div>
+				</div>
 				<div className={styles["right-foot"]}>Time and Date</div>
 			</div>
 		);

0379297a3eb2a7fb2d8d74bf4d7f5aa1d03c8201
Lisander Lopez <lisander@buffalo.edu>
2020-07-11 22:45:13 -0400

    Social Desktop Items work #12 #13 #14

---
+++ components/actions/DataActions.js
@@ -0,0 +1,13 @@
+import DATA from "../../data.json";
+
+export function getLinkedIn() {
+	return DATA.LinkedIn;
+}
+
+export function getEmail() {
+	return "mailto:" + DATA.Email;
+}
+
+export function getGitHub() {
+	return DATA.GitHub;
+}
--- components/desktop/Desktop.js
+++ components/desktop/Desktop.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import styles from "../../css/desktop.module.scss";
+
+import { getEmail, getGitHub, getLinkedIn } from "../actions/DataActions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faFolder,
@@ -13,30 +15,60 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 export default function Desktop(props) {
 	return (
 		<div className={styles["grid-container"] + " " + props.className}>
-			<div className={styles["grid-project"]}>
+			<div
+				className={styles["grid-project"]}
+				onClick={() => {
+					handleProject();
+				}}
+			>
 				<FontAwesomeIcon icon={faFolder} size="4x" fixedWidth />
 				<span>Projects</span>
 			</div>
-			<div className={styles["grid-resume"]}>
+			<div
+				className={styles["grid-resume"]}
+				onClick={() => {
+					handleResume();
+				}}
+			>
 				<FontAwesomeIcon icon={faFilePdf} size="4x" fixedWidth />
 				<span>Resume.pdf</span>
 			</div>
-			<div className={styles["grid-trash"]}>
+			<div
+				className={styles["grid-trash"]}
+				onClick={() => {
+					handleRecycle();
+				}}
+			>
 				<FontAwesomeIcon icon={faTrashAlt} size="4x" fixedWidth />
 				<span>Recycle Bin</span>
 			</div>
 
-			<div className={styles["grid-email"]}>
+			<div
+				className={styles["grid-email"]}
+				onClick={() => {
+					window.open(getEmail(), "_blank");
+				}}
+			>
 				<FontAwesomeIcon icon={faEnvelope} size="4x" fixedWidth />
 				<span>Email</span>
 			</div>
 
-			<div className={styles["grid-github"]}>
+			<div
+				className={styles["grid-github"]}
+				onClick={() => {
+					window.open(getGitHub(), "_blank");
+				}}
+			>
 				<FontAwesomeIcon icon={faGithub} size="4x" fixedWidth />
 				<span>GitHub</span>
 			</div>
 
-			<div className={styles["grid-linkedin"]}>
+			<div
+				className={styles["grid-linkedin"]}
+				onClick={() => {
+					window.open(getLinkedIn(), "_blank");
+				}}
+			>
 				<FontAwesomeIcon icon={faLinkedin} size="4x" fixedWidth />
 				<span>LinkedIn</span>
 			</div>

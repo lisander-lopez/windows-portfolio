cae3e1a1cbff11c27718c17ddeff3bf8822c46f8
Lisander Lopez <lisander@buffalo.edu>
2020-07-17 00:05:26 -0400

    Program switching added! #20

--- components/desktop/Program.js
+++ components/desktop/Program.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowMinimize, faTimes } from "@fortawesome/free-solid-svg-icons";
 
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
 import styles from "../../css/program.module.scss";
 
 export default function Program({ app }) {
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.header}>
-				Header
-				{app.name}
+				<div className={styles["header-left"]} />
+				<div className={styles["header-middle"]}>{app.name}</div>
+				<div className={styles["header-right"]}>
+					<div className={styles["minimize-wrap"]}>
+						<FontAwesomeIcon icon={faWindowMinimize} size="1x" />
+					</div>
+					<div className={styles["maximize-wrap"]}>
+						<FontAwesomeIcon icon={faSquare} size="1x" />
+					</div>
+					<div className={styles["close-wrap"]}>
+						<FontAwesomeIcon icon={faTimes} size="1x" />
+					</div>
+				</div>
 			</div>
-			<div className="body">
+			<div className={styles.body}>
 				<div className="container">Container</div>
 			</div>
 		</div>

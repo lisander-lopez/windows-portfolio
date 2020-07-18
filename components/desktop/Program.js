c30687c4dc541de7c123d8b0aa83bb6a9455e3b0
Lisander Lopez <lisander@buffalo.edu>
2020-07-17 20:08:08 -0400

    #25 completed

--- components/desktop/Program.js
+++ components/desktop/Program.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowMinimize, faTimes } from "@fortawesome/free-solid-svg-icons";
 
@@ -6,35 +7,48 @@ import { faSquare } from "@fortawesome/free-regular-svg-icons";
 import styles from "../../css/program.module.scss";
 import { useAppList } from "../context/AppListState";
 
+// Took this code from https://stackoverflow.com/questions/2332811/capitalize-words-in-string/7592235#7592235
+
+const capitalize = (s) => {
+	return s.toLowerCase().replace(/\b./g, (a) => {
+		return a.toUpperCase();
+	});
+};
+
 export default function Program({ app }) {
 	const [appList, dispatch] = useAppList();
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.header}>
-				<div className={styles["header-left"]} />
-				<div className={styles["header-middle"]}>{app.name}</div>
-				<div className={styles["header-right"]}>
-					<div className={styles["minimize-wrap"]}>
-						<FontAwesomeIcon icon={faWindowMinimize} size="1x" />
-					</div>
-					<div className={styles["maximize-wrap"]}>
-						<FontAwesomeIcon icon={faSquare} size="1x" />
-					</div>
-					<div
-						className={styles["close-wrap"]}
-						onClick={() => {
-							dispatch({ name: app.name, type: "close" });
-						}}
-						role="button"
-						tabIndex={0}
-					>
-						<FontAwesomeIcon icon={faTimes} size="1x" />
+		<>
+			<Head>
+				<title>{`${capitalize(app.name)} - Lisander Lopez`}</title>
+			</Head>
+			<div className={styles.wrapper}>
+				<div className={styles.header}>
+					<div className={styles["header-left"]} />
+					<div className={styles["header-middle"]}>{app.name}</div>
+					<div className={styles["header-right"]}>
+						<div className={styles["minimize-wrap"]}>
+							<FontAwesomeIcon icon={faWindowMinimize} size="1x" />
+						</div>
+						<div className={styles["maximize-wrap"]}>
+							<FontAwesomeIcon icon={faSquare} size="1x" />
+						</div>
+						<div
+							className={styles["close-wrap"]}
+							onClick={() => {
+								dispatch({ name: app.name, type: "close" });
+							}}
+							role="button"
+							tabIndex={0}
+						>
+							<FontAwesomeIcon icon={faTimes} size="1x" />
+						</div>
 					</div>
 				</div>
+				<div className={styles.body}>
+					<div className="container">Container</div>
+				</div>
 			</div>
-			<div className={styles.body}>
-				<div className="container">Container</div>
-			</div>
-		</div>
+		</>
 	);
 }

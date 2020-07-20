835ebf04953f371c8afa7aba4dd56c586cd00687
Lisander Lopez <lisander@buffalo.edu>
2020-07-20 01:50:21 -0400

    Dev runs good on WSL its not slow... #21 Responsive and scroll style is missing #9 Added Projects #6

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styles from "./portfolio.module.scss";
+import styles from "../../css/portfolio.module.scss";
 
 import { getProjects } from "../actions/DataActions";
 
@@ -8,19 +8,41 @@ export default function Portfolio() {
 
 	return (
 		<div className={styles.grid}>
-			<div className={styles.header}>
-				<span>Name</span>
-				<span>Description</span>
-				<span>Type</span>
-				<span>Date Created</span>
+			<div className={styles.header + " " + styles.row}>
+				<div className={styles.cell}>
+					<span>Name</span>
+				</div>
+				<div className={styles.cell}>
+					<span>Description</span>
+				</div>
+				<div className={styles.cell}>
+					<span>Link</span>
+				</div>
+				<div className={styles.cell}>
+					<span>Created At</span>
+				</div>
 			</div>
 			{items.map((item) => {
 				return (
-					<div key={item.title}>
-						<span>{item.title}</span>
-						<span>{item.description}</span>
-						<span>{item.link}</span>
-						<span>{item.created}</span>
+					<div
+						key={item.title}
+						className={styles.row}
+						onClick={() => {
+							window.open(item.link.url, "_blank");
+						}}
+					>
+						<div className={styles.cell}>
+							<span>{item.title}</span>
+						</div>
+						<div className={styles.cell}>
+							<span>{item.description}</span>
+						</div>
+						<div className={styles.cell}>
+							<span>{item.link.type}</span>
+						</div>
+						<div className={styles.cell}>
+							<span>{item.created}</span>
+						</div>
 					</div>
 				);
 			})}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
+const WebpackBar = require("webpackbar");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
 	enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+	webpack: (config) => {
+		config.plugins.push(
+			new WebpackBar({
+				fancy: true,
+				profile: true,
+				basic: false,
+			})
+		);
+
+		return config;
+	},
+});

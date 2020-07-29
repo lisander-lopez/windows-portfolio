88f0c1942c5ff871735e71e14f25cf2af08439f0
Lisander Lopez <lisander@buffalo.edu>
2020-07-28 21:58:17 -0400

    have to fix Books API Retrieval. Might switch to googles book API #15 #2

--- components/footer/aboutme/BooksILove.js
+++ components/footer/aboutme/BooksILove.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getBooks } from "../../actions/DataActions";
 
 import styles from "../../../css/booksilove.module.scss";
 
 function BooksILove() {
+	const [books, setBooks] = useState({
+		data: null,
+	});
+
+	useEffect(() => {
+		const func = async () => {
+			console.log("BLAHH");
+			let apiRequestData = "";
+			getBooks().map((book) => {
+				apiRequestData += `ISBN:${book},`; // Trailing , is fine
+			});
+			//https://www.googleapis.com/books/v1/volumes?q=isbn:0593084683
+			const res = await fetch(
+				`https://www.googleapis.com/books/v1/volumes?q=${apiRequestData}`
+			);
+			const data = await res.json();
+			setBooks({ data: data });
+		};
+		func();
+	}, []);
+	console.log(books);
 	return (
 		<div className={styles.container}>
-			{getBooks().map((book) => {
-				return <div>{book}</div>;
-			})}
+			{
+				books.data && "yes"
+
+				// Object.keys(books.data).map((key) => {
+				// 	console.log("Books", books);
+				// 	console.log("Key", key);
+				// 	console.log(books.data[key]);
+				// 	const imgSrc = books.data[key].cover
+				// 		? books.data[key].cover.small
+				// 		: "#";
+				// 	return (
+				// 		<div key={key}>
+				// 			<span>{books.data[key].title}</span>
+				// 			<img src={imgSrc || "#"} alt="Book Cover" />
+				// 		</div>
+				// 	);
+				// })
+			}
 		</div>
 	);
 }

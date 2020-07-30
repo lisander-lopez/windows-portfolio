ea770c5ae42cb312f7596fd5d7c2692b8f1fbfc8
Lisander Lopez <lisander@buffalo.edu>
2020-07-30 01:57:48 -0400

    #32

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,15 @@ class MyDocument extends Document {
 
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
+					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+					<meta
+						name="description"
+						content="Lisander Lopez's Personal Portfolio site based on Windows Desktop UI."
+					/>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 					<link
 						href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
 						rel="stylesheet"

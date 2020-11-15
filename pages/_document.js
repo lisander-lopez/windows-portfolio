e60ce7bc843dcf3659864dc60fb43aaa5718a059
Lisander Lopez <lisander@sutdiolabs.com>
2020-11-15 16:28:00 -0500

    Meta Tags should not be in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx) {
@@ -9,19 +9,6 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-					<meta
-						name="description"
-						content="Lisander Lopez's Personal Portfolio site based on Windows Desktop UI."
-					/>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-
-					<link
-						href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
 				<body>
 					<Main />
 					<NextScript />
